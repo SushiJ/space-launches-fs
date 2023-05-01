761bc6f343a9ff431e7fca3c714c29c618ca81dd
Sushij <sushant.wurk@gmail.com>
2023-05-01 17:48:10 +0530

    Tests

--- server/__tests__/launches.test.ts
+++ server/__tests__/launches.test.ts
@@ -3,14 +3,55 @@ import app from "../src/app";
 
 describe("Test GET /launches", () => {
   test("It should respond with 200 success", async () => {
-    const response = await request(app).get("/launches");
-    expect(response.status).toBe(200);
+    await request(app)
+      .get("/launches")
+      .expect(200)
+      .expect("Content-Type", /json/);
   });
 });
 
 describe("Test POST /launch", () => {
-  test("It should respond with 200 success", () => {});
+  test("It should respond with 201 created", async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send({
+        mission: "USS Enterprise",
+        rocket: "Some rocket",
+        destination: "Kepler-235 e",
+        launchDate: new Date("January 5, 2028").toISOString(),
+      })
+      .expect(201)
+      .expect("Content-Type", /json/);
 
-  test("It should catch missing required properties", () => {});
-  test("It should catch invalid dates", () => {});
+    expect(response.body).toMatchObject({
+      mission: "USS Enterprise",
+      rocket: "Some rocket",
+      destination: "Kepler-235 e",
+      launchDate: new Date("January 5, 2028").toISOString(),
+    });
+  });
+
+  test("It should catch missing required properties and respond with 400 bad request", async () => {
+    await request(app)
+      .post("/launches")
+      .send({
+        mission: "USS Enterprise",
+        rocket: "Some rocket",
+        destination: "Kepler-235 e",
+      })
+      .expect("Content-Type", /json/)
+      .expect(400);
+  });
+  test("It should catch invalid dates and respond with 400 bad request", async () => {
+    await request(app)
+      .post("/launches")
+      .send({
+        mission: "USS Enterprise",
+        rocket: "Some rocket",
+        destination: "Kepler-235 e",
+        launchDate: "January 5, 2028",
+      })
+      .expect("Content-Type", /json/)
+      .expect(400);
+  });
 });
