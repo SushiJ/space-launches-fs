cc8d2674090af26047546ca09c771297b3c28bbf
Sushij <sushant.wurk@gmail.com>
2023-06-22 23:23:42 +0530

    client. added footer section

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,11 @@
+// TODO: FOOTER
+function Footer() {
+  return (
+    <div class="">
+      <hr class={"border-emerald-400 border-[1px]"} />
+      Footer
+    </div>
+  )
+}
+
+export default Footer;
--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -1,12 +1,12 @@
 import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 function Layout({ children }: { children: any }) {
   return (
-    <div className="bg-gray-900 h-auto">
-      <header className="bg-gray-900 text-gray-100 max-w-6xl mx-auto">
-        <Navbar />
-      </header>
-      <div className="max-w-6xl mx-auto pb-4 h-auto">{children}</div>
+    <div className="bg-gray-900 bg-transparent h-screen flex flex-col max-w-6xl mx-auto">
+      <Navbar />
+      <div className="max-w-6xl mx-auto pb-4 flex-1">{children}</div>
+      <Footer />
     </div>
   );
 }
