34b8fea742b5725e4db563ad779a32c60020724f
Sushij <sushant.wurk@gmail.com>
2023-09-10 00:12:06 +0530

    wip footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,11 +1,48 @@
 // TODO: FOOTER
+import { Link } from "preact-router";
+import { HistoryIcon, LaunchIcon, UpcomingIcon } from "./Icons";
+import { Logo } from "./Logo";
+
 function Footer() {
   return (
-    <div class="">
-      <hr class={"border-emerald-400 border-[1px]"} />
-      Footer
-    </div>
-  )
+    <>
+      <hr class="border-emerald-400 border-[1px]" />
+      <nav className="flex items-center py-8 bg-transparent mx-auto">
+        <Link className="flex items-center space-x-4 text-2xl" href="/">
+          <Logo />
+        </Link>
+        <div className="text-2xl">
+          <Link
+            className="flex items-center hover:text-emerald-400"
+            href="/launches"
+          >
+            <span className="mx-1">
+              <LaunchIcon />
+            </span>
+            Launch
+          </Link>
+          <Link
+            className="flex items-center hover:text-emerald-400"
+            href="/upcoming"
+          >
+            <span className="mx-1">
+              <UpcomingIcon />
+            </span>
+            Upcoming
+          </Link>
+          <Link
+            className="flex items-center hover:text-emerald-400"
+            href="/history"
+          >
+            <span className="mx-1">
+              <HistoryIcon />
+            </span>
+            History
+          </Link>
+        </div>
+      </nav>
+    </>
+  );
 }
 
 export default Footer;
