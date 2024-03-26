5fd5cb76389964c1820e5319150e7142c6af5396
Sushant Sharma <sushant.wurk@gmail.com>
2024-03-26 23:46:00 +0530

    Chore: Refactor init (seedDB)

--- server/src/app.ts
+++ server/src/app.ts
@@ -5,8 +5,8 @@ import cors from "cors";
 // setLogLevel("TRACE");
 import { router } from "./routes";
 import connectDB from "./utils/dbConnect";
-import { parseCsv } from "./models/planets";
-import { loadSpaceXData } from "./models/launches";
+// import { parseCsv } from "./models/planets";
+// import { loadSpaceXData } from "./models/launches";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -14,31 +14,15 @@ const PORT = process.env.PORT || 3000;
 app.use(
   cors({
     origin: "http://localhost:5173",
-  })
+  }),
 );
 app.use(morgan("combined"));
 app.use(express.json());
 app.use(router.planetsRouter).use(router.launchesRouter);
 
-app.listen(PORT, () => {
-  connectDB()
-    .then(() => {
-      console.log("connected To DB");
-      loadSpaceXData()
-        .then(() => {
-          console.log("Successfully populated launches");
-        })
-        .catch((e) => {
-          console.log("Failed to populate launches", e);
-        });
-      parseCsv()
-        .then(() => {
-          console.log("Parsed");
-          console.log(`Server up at http://localhost${PORT}`);
-        })
-        .catch((e) => console.log(e));
-    })
-    .catch((e) => console.error("Failed to connect", e));
+app.listen(PORT, async () => {
+  console.log("Server up", PORT);
+  await connectDB();
 });
 
 export default app;
--- server/src/utils/dbConnect.ts
+++ server/src/utils/dbConnect.ts
@@ -2,5 +2,5 @@ import mongoose from "mongoose";
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/nasa?retryWrites=true&w=majority";
 export default async function connectDB() {
-  await mongoose.connect(MONGO_URL);
+  await mongoose.connect(MONGO_URL).then(() => console.log("Connected"));
 }
